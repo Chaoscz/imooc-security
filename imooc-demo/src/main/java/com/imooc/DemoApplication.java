0a37e17b302a1190595270f4d1bf85303f079859
chaoscz <756493831@qq.com>
2018-07-10 21:00:26 +0800

    fix

--- imooc-demo/src/main/java/com/imooc/DemoApplication.java
+++ imooc-demo/src/main/java/com/imooc/DemoApplication.java
@@ -2,13 +2,13 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@ComponentScan("com.imooc.*")
+
 public class DemoApplication {
     public static void main(String[] args){
         SpringApplication.run(DemoApplication.class,args);
